910e715656152f9379d5f03072e856c9d405e218
Lluis <lluisyast@gmail.com>
2013-12-17 16:58:57 +0100

    Eliminadas partes innecesarias. La frecuencia se muestra solo cuando actualizamos su valor

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -3,6 +3,7 @@
 
 volatile uint32_t cuenta = 0;
 volatile uint8_t i = 0;
+volatile bool listo = false;
 
 void setup()
 {
@@ -17,9 +18,6 @@ void setup()
 
 	while(!OSCCONbits.SOSCRDY);
 
-	TRISAbits.TRISA3 = 0;
-	LATAbits.LATA3 = 0;
-
 
 	OpenTimer1(T1_ON | T1_PS_1_1 | T1_SOURCE_EXT | T1_SYNC_EXT_OFF, 0xFFFF);
 	
@@ -28,34 +26,29 @@ void setup()
 	IPC4SET = 0x00000001; // Subprioridad 1
 	IFS0CLR = 0x00010000; // Clear timer interrupt status flag
 	IEC0SET = 0x00010000; // Habilita inturrupciones de los temporizadores
-
-	TRISCbits.TRISC3 = 1;
 }
 
 void loop()
 {
-	Serial1.write(0xFE);
-	Serial1.write(0x01);
-	Serial1.print("Freq: ");
-	Serial1.print(cuenta * 5);
-	Serial1.write(0xFE);
-	Serial1.write(0x8E);
-	Serial1.print("Hz");
-	delay(500);
+	if(listo) {
+		listo = false;
+		Serial1.write(0xFE);
+		Serial1.write(0x01);
+		Serial1.print("Freq: ");
+		Serial1.print(cuenta * 5);
+		Serial1.write(0xFE);
+		Serial1.write(0x8E);
+		Serial1.print("Hz");
+	}
 }
 
 extern "C" {
 	void __ISR(_TIMER_4_VECTOR,ipl3)Timer4Handler(void)
 	{
-		if (i >= 5) {
-			PORTAINV = 0x0008;
-			i = 0;
-		} else {
-			i++;
-		}
 		cuenta = TMR1;
 		TMR1 = 0x0;
 		IFS0CLR = 0x00010000;
+		listo = true;
 	}
 	// void __ISR(_TIMER_1_VECTOR,ipl3)Timer1Handler(void)
 	// {
